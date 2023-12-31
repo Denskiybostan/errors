fe1fd5343aaad8d2c45772d2802857d4ced6a0c6
denis <deniz246@mail.ru>
2023-12-31 20:32:46 +0300

    выполнение задания начато

---
+++ src/main/java/authorization/sky/authorizationdemo/AuthorizationDemoApplication.java
@@ -0,0 +1,13 @@
+package authorization.sky.authorizationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationDemoApplication.class, args);
+	}
+
+}
